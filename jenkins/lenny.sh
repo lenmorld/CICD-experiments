c53f95b61babc11bbe3309a96b990f823cbefc9d
lenmorld <lenmorld@gmail.com>
2022-10-10 12:48:41 -0400

    CI - trigger

--- jenkins/lenny.sh
+++ jenkins/lenny.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "Hello world"
+
+export LENNY_VERSION=0.20.49
+envsubst < kubernetes/lenny.yaml >> kubernetes/lenny
+cat kubernetes/lenny > kubernetes/lenny.yaml
+cat kubernetes/lenny.yaml
