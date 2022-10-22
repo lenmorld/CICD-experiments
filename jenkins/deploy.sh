e7f89679b7422bbdd39a456d04ca64efc6ba5798
lenmorld <lenmorld@gmail.com>
2022-10-22 09:29:21 -0400

    fix envsubst

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -5,7 +5,10 @@ echo "Hello world"
 echo "Image version: $1"
 export IMAGE_VERSION="$1"
 printenv
-envsubst < kubernetes/deployment.yaml >> kubernetes/deployment
+envsubst < kubernetes/deployment.yaml > kubernetes/deployment
+echo "===== result of envsubst ===="
+cat kubernetes/deployment
+echo "===== end of envsubst"
 cat kubernetes/deployment > kubernetes/deployment.yaml
 cat kubernetes/deployment.yaml
 rm kubernetes/deployment
